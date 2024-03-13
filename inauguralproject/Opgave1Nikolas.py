fe6d1b06ad45e9f6b33cde62ab6506e411a9512f
NikolasLindgaard <Rlw277@alumni.ku.dk>
2024-03-13 17:35:45 +0100

    ny

--- inauguralproject/Opgave1Nikolas.py
+++ inauguralproject/Opgave1Nikolas.py
@@ -1,12 +1,3 @@
-alpha = 1/3
-beta = 2/3
-
-
-w1A = 0.8
-w2A = 0.3
-p2 = 1
-w1B = 1-w1A
-w2B = 1-w2A
 from types import SimpleNamespace
 
 class InauguralprojectClass:
@@ -22,21 +13,24 @@ class InauguralprojectClass:
         # b. endowments
         par.w1A = 0.8
         par.w2A = 0.3
+        par.w1B = 1-self.par.w1A
+        par.w2B = 1-self.par.w2A
         par.p2 = 1
 
     def utility_A(self,x1A,x2A):
-        uA = x1A**alpha*x2A**(1-alpha)
+        return x1A**self.par.alpha*x2A**(1-self.par.alpha)
+        
 
     def utility_B(self,x1B,x2B):
-        uB = x1B**beta*x2B**(1-beta)
+        return x1B**self.par.beta*x2B**(1-self.par.beta)
+        
 
     def demand_A(self,p1):
-        IA = p1*w1A+p2*w2A
-        return alpha*IA/p1
+        return self.par.alpha((p1*self.par.w1A+self.par.p2*self.par.w2A)/p1)
+        
 
     def demand_B(self,p1):
-        IB = p1*w1B+p2*w2B
-        return beta*IB/p2
+        return (1-self.par.alpha)((p1*self.par.w1B+self.par.p2*self.par.w2B)/p2)
 
 
     def check_market_clearing(self,p1):
