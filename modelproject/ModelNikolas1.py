c55a2557d8aac05d6d2cc8b09f470213d45b51fc
NikolasLindgaard <Rlw277@alumni.ku.dk>
2024-05-10 11:36:56 +0200

    isnds

--- modelproject/ModelNikolas1.py
+++ modelproject/ModelNikolas1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ASAD: 
     # Class for the AS-AD model with initial values for the parameters
-    def __init__(self, T, alpha=0.7, gamma=0.075, tol=0.01, z=0, s=0, z_duration=0, s_duration=0): 
+    def __init__(self, T, alpha=0.5, gamma=0.07, tol=0.02, z=0, s=0, z_duration=0, s_duration=0): 
         self.alpha = alpha 
         self.gamma = gamma 
         self.tol = tol #tolerance
@@ -12,21 +12,10 @@ class ASAD:
         self.s = s #shock to aggregate supply
         self.z_duration = z_duration #duration of shock to aggregate demand
         self.s_duration = s_duration #duration of shock to aggregate supply
-        self.delta = 0.97 #discount factor
+        self.delta = 0.95 #discount factor
         
     def solve_model(self):
         
-        """ Solves the model using the initial values for the parameters 
-        
-        args: 
-        self (class): class with initial values for the parameters
-        
-        Returns:
-        self.yhat_vec (list): list of output gaps
-        self.pihat_vec (list): list of inflation gaps
-        self.t_vec (list): list of periods
-        
-        """
         # a. Create empty lists
         
         self.yhat_vec = []
@@ -56,44 +45,22 @@ class ASAD:
             self.pihat_vec.append(pihat)
             self.t_vec.append(t)
          
-    def plot_results(self): #plots the results
+    def plot_results(self): 
         
         plt.figure(figsize=(10, 6))
-        plt.plot(self.t_vec, self.yhat_vec, label="Output gap")
-        plt.plot(self.t_vec, self.pihat_vec, label="Inflation gap")
+        plt.plot(self.t_vec, self.yhat_vec, label="Output-gap")
+        plt.plot(self.t_vec, self.pihat_vec, label="Inflation-gap")
         plt.xlabel("Periods")
         plt.ylabel("Gap")
-        plt.title("Figure 1: Output gap and Inflation gap")
+        plt.title("Figure 1: Output-gap and Inflation-gap")
         plt.legend()
         plt.show()
         
     def plot_ad_as(self):
-        
-        """ Creating and plotting the AD and AS curves of the given output and inflation gaps
-        args:
-        self (class): class with initial values for the parameters
-        
-        Returns: 
-        
-        original_LRAD (list): list of original LRAD curve
-        lras_curve (list): list of LRAS curve
-        plot (plot): plot of AD and AS curves
 
-        
-        Returns: Temporary shocks:
-        
-        ad_curve_t (list): list of AD curves
-        as_curve_t (list): list of AS curves
-        
-        Returns: Permenant shocks:
-        
-        adjusted_LRAD (list): list of adjusted LRAD curve
-        LRAS2 (list): list of LRAS2 curve
-        
-        """
         # a. Create y_values & pi_hat
         
-        y_values = np.linspace(-0.01, 0.01, 100)
+        y_values = np.linspace(-0.02, 0.02, 100)
         pi_hat = self.pihat_vec
         # b. Create functions for AD and AS curves
         
@@ -112,20 +79,18 @@ class ASAD:
                 s_t = 0
             return pi_1 + gamma * y + s_t
 
-        # Initiate plot
         
         plt.figure(figsize=(10, 6))
-        plt.axvline(x=0, color="red", label="LRAS curve") #LRAS curve
+        plt.axvline(x=0, color="blue", label="LRAS-curve") 
         
         # The case for short-term shocks
         if self.s_duration < self.T:
             
-            plt.axhline(y=0, color='black', linestyle=':', label='Long Run Inflation Gap = 0') #LRAS curve
-            for t in range(self.T): #plotting AD curve for each period
+            plt.axhline(y=0, color='black', linestyle=':', label='Long Run Inflation-Gap = 0')            for t in range(self.T): #plotting AD curve for each period
                 
                 ad_curve_t = ad_function(self.alpha, y_values, t, self.z, self.z_duration)
-                plt.plot(y_values, ad_curve_t, color="blue")
-            # Creating and plotting the SRAS curves
+                plt.plot(y_values, ad_curve_t, color="green")
+
             
             for t in range(self.T):
                 if t == 0:
@@ -134,17 +99,14 @@ class ASAD:
                     pi_1 = pi_hat[t-1]
                     
                 as_curve_t = as_function(self.alpha, pi_1, self.gamma, y_values, t, self.s, self.s_duration)
-                plt.plot(y_values, as_curve_t, color="red")
-
-            # Original LRAD curve when s_duration < self.T
-            
+                plt.plot(y_values, as_curve_t, color="blue")       
             original_LRAD = ad_function(self.alpha, y_values, 0, 0, self.z_duration)
-            plt.plot(y_values, original_LRAD, color="blue", label="Original LRAD curve")
-        # The case for permanent shocks
+            plt.plot(y_values, original_LRAD, color="green", label="Original LRAD-curve")
+
         
         if self.s_duration == self.T:
-            plt.axhline(y=0, color='black', linestyle=':', label='Long Run Inflation Gap = 0')
-            # Original LRAD curve
+            plt.axhline(y=0, color='black', linestyle=':', label='Long Run Inflation-gap = 0')
+
             
             original_LRAD = ad_function(self.alpha, y_values, 0, 0, self.z_duration)
             plt.plot(y_values, original_LRAD, color="blue", label="Original LRAD curve")
@@ -165,8 +127,8 @@ class ASAD:
         
         plt.annotate(r'$\overline{y}$', xy=(-0.0015, -0.0125), fontsize=12)
         plt.annotate(r'$\overline{\pi}$', xy=(-0.0105, -0.0015), fontsize=12)
-        plt.xlabel(r'Output gap $(\hat{y})$')
-        plt.ylabel(r'Inflation gap $(\hat{\pi})$')
+        plt.xlabel(r'Output-gap $(\hat{y})$')
+        plt.ylabel(r'Inflation-gap $(\hat{\pi})$')
         
         if self.z_duration > 0 and self.z_duration < self.T:
             plt.title(f"Figure 2: {self.z_duration} period positive demand shock")
@@ -177,20 +139,7 @@ class ASAD:
         plt.show()
         
     #Stochastic shocks
-    def solve_stochastic_shock(self, seed):
-        """
-        args:
-        self (class): class with initial values for the parameters
-        seed (int): seed for random number generator
-
-        returns:
-        
-        self.yhat_vec_stoc (list): list of output gaps
-        self.pi_hat_vec_stoc (list): list of inflation gaps
-        self.t_vec (list): list of periods
-         
-        """
-        # a. creaat empty lists % setting parameters
+    def solve_stochastic_shock(self, seed): 
         
         self.z_vector = []
         self.s_vector = []
@@ -234,27 +183,3 @@ class ASAD:
             self.yhat_vec_stoc.append(yhat)
             self.pihat_vec_stoc.append(pihat)
             self.t_vec.append(t)
-
-    # creating social loss function & calculating social loss   
-    def social_loss_function(self):
-        social_loss = 0
-        for t in range(len(self.yhat_vec_stoc)): # Loop through all periods as the discount factor is correctly applied
-            social_loss += self.delta**t * (self.yhat_vec_stoc[t]**2 + self.pihat_vec_stoc[t]**2)
-        return social_loss
-    
-    # social loss function with alpha as input
-    def social_loss_alpha(self, alpha_value, seed):
-        self.alpha = alpha_value # Update the alpha value
-        self.solve_model() # Solve the model
-        self.solve_stochastic_shock(seed) # Solve the model with stochastic shock
-        return self.social_loss_function() # Return the social loss function
-    
-    def average_social_loss(alpha_value, n_simulations=500): # Average social loss function
-        social_losses = [] # Empty list for social losses
-        for i in range(n_simulations): # Loop through all simulations
-            seed = i # Set seed
-            asad_model = ASAD(T=100, alpha=alpha_value ,gamma=0.075) # Create model
-            asad_model.solve_model() # Solve model
-            asad_model.solve_stochastic_shock(seed) # Solve model with stochastic shock
-            social_losses.append(asad_model.social_loss_function()) # Append social loss to list
-        return np.mean(social_losses) # Return average social loss
