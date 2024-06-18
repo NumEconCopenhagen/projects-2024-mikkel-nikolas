6e40dcf8842f8026c3fcb497ddd48cf36299be48
NikolasLindgaard <Rlw277@alumni.ku.dk>
2024-06-18 15:25:03 +0200

    inauguralproject

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,6 +1,5 @@
 from types import SimpleNamespace
-
-
+import numpy as np
 class InauguralprojectClass:
     def __init__(self, alpha=1/3, beta=2/3, w1A=0.8, w2A=0.3, p2=1):
         self.alpha = alpha
@@ -41,10 +40,29 @@ class InauguralprojectClass:
         eps2 = x2A - self.w2A + x2B - self.w2B
         return eps1, eps2
     
-    def max_u_a(self,x):
+    def max_u_a(self, x):
         return -self.utility_A(x[0], x[1])
 
-    def max_u_ab(self,x):
+    def max_u_ab(self, x):
         utility_A = self.utility_A(x[0], x[1])
         utility_B = self.utility_B(1-x[0], 1-x[1])
         return -(utility_A + utility_B) 
+    
+    def find_equilibrium(self, w1A, w2A):
+        p1_range = np.linspace(0.01, 3, 100)
+        market_clearing = []
+        for p1 in p1_range:
+            self.w1A = w1A
+            self.w2A = w2A
+            self.w1B = 1 - w1A
+            self.w2B = 1 - w2A
+            x1A, x2A = self.demand_A(p1)
+            x1B, x2B = self.demand_B(p1)
+            epsilon1 = abs((x1A + x1B) - 1)
+            epsilon2 = abs((x2A + x2B) - 1)
+            market_clearing.append(epsilon1 + epsilon2)
+        min_index = np.argmin(market_clearing)
+        p1_eq = p1_range[min_index]
+        x1A, x2A = self.demand_A(p1_eq)
+        x1B, x2B = self.demand_B(p1_eq)
+        return x1A, x2A, x1B, x2B, p1_eq
