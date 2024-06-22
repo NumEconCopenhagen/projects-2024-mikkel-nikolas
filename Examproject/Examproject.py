7514656a37a5c5c31e7a2c33aa68a2fb777f648a
zvg359 <zvg359@alumni.ku.dk>
2024-06-22 11:19:26 +0200

    un

--- Examproject/Examproject.py
+++ Examproject/Examproject.py
@@ -15,6 +15,9 @@ class ProductionEconomy:
     def firm_output(self, w, pj):
         return self.A * self.firm_labor_demand(w, pj) ** self.gamma
     
+    def firm_output_p2(self, w, p2):
+        return self.A * self.firm_labor_demand(w, p2) ** self.gamma
+
     def firm_profit(self, w, pj):
         return ((1-self.gamma) / self.gamma) * w * self.firm_labor_demand(w, pj)
 
@@ -35,4 +38,28 @@ class ProductionEconomy:
                                      ((1 - self.alpha) * income(ell) / (p2 + tau)) ** (1 - self.alpha)) - \
                               self.nu * ell ** (1 + self.epsilon) / (1 + self.epsilon)
         return max((utility(ell), ell) for ell in np.linspace(0, 10, 1000))[1]
-    
+
+    def C1_star(self, p1, p2, w, tau, T):
+        ell_star = self.consumer_optimal_labor(p1, p2, w, tau, T)
+        c1, _, _ = self.consumer_budget(p1, p2, w, tau, T, ell_star)
+        return c1
+
+    def C2_star(self, p1, p2, w, tau, T):
+        ell_star = self.consumer_optimal_labor(p1, p2, w, tau, T)
+        _, c2, _ = self.consumer_budget(p1, p2, w, tau, T, ell_star)
+        return c2
+
+    def calculate_T(self, p1, p2, w, tau):
+        c2_star = self.C2_star(p1, p2, w, tau, tau * 0)  # T is initially 0
+        return tau * c2_star
+
+    def SWF_function(self, p1, p2, w, tau, kappa):
+        T = self.calculate_T(p1, p2, w, tau)
+        ell_opt = self.consumer_optimal_labor(p1, p2, w, tau, T)
+        c1, c2, _ = self.consumer_budget(p1, p2, w, tau, T, ell_opt)
+        consumer_utility = self.consumer_utility(c1, c2, ell_opt)
+        firm_output_p2 = self.firm_output_p2(w, p2)
+        return consumer_utility - kappa * firm_output_p2
+
+
+ 
